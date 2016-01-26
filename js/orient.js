45ad288a9df455f06fbf84346f43fda0cdb6132f
arijitlaik <arijit.netcom@gmail.com>
2016-01-26 18:31:22 +0530

    single call to get location and other ui fixes including location edit button and fab for saverbutton

--- js/orient.js
+++ js/orient.js
@@ -1,81 +1,109 @@
- init();
+getLocation();
+getOrienation();
 
- function init() {
- 	if (window.DeviceOrientationEvent) {
- 		//document.getElementById("doEvent").innerHTML = "DeviceOrientation";
- 		// Listen for the deviceorientation event and handle the raw data
- 		window.addEventListener('deviceorientation', function (eventData) {
- 			// gamma is the left-to-right tilt in degrees, where right is positive
- 			var rake = eventData.gamma;
+function getOrienation() {
+	if (window.DeviceOrientationEvent) {
+		//document.getElementById("doEvent").innerHTML = "DeviceOrientation";
+		// Listen for the deviceorientation event and handle the raw data
+		window.addEventListener('deviceorientation', function (eventData) {
+			// gamma is the left-to-right tilt in degrees, where right is positive
+			var rake = eventData.gamma;
 
- 			// beta is the front-to-back tilt in degrees, where front is positive
- 			var dip = eventData.beta;
+			// beta is the front-to-back tilt in degrees, where front is positive
+			var dip = eventData.beta;
 
- 			// alpha is the compass direction the device is facing in degrees
- 			var strike = eventData.alpha;
+			// alpha is the compass direction the device is facing in degrees
+			var strike = eventData.alpha;
 
- 			// call our orientation event handler
-			getLocation();
- 			deviceOrientationHandler(strike, dip, rake);
- 		}, false);
- 	} else {
- 		//document.getElementById("doEvent").innerHTML = "Not supported on your device or browser.  Sorry."
- 	}
- }
+			// call our orientation event handler
 
- //degree to radian torad(degree)
- function torad(degrees) {
- 	return degrees * Math.PI / 180;
- }
- //radian to degree todeg(radian)
- function todeg(radians) {
- 	return radians * 180 / Math.PI;
- }
+			deviceOrientationHandler(strike, dip, rake);
+		}, false);
+	} else {
+		//document.getElementById("doEvent").innerHTML = "Not supported on your device or browser.  Sorry."
+	}
+}
 
- function deviceOrientationHandler(strike, dip, rake) {
+//degree to radian torad(degree)
+function torad(degrees) {
+	return degrees * Math.PI / 180;
+}
+//radian to degree todeg(radian)
+function todeg(radians) {
+	return radians * 180 / Math.PI;
+}
 
- 	document.getElementById("strike").parentNode.className += " is-focused";
- 	document.getElementById("strike").value = strike;
- 	document.getElementById("dip").parentNode.className += " is-focused"
- 	document.getElementById("dip").value = dip;
- 	var tp = sdrtotp(strike, dip, rake);
- 	document.getElementById("trend").parentNode.className += " is-focused";
- 	document.getElementById("trend").value = tp[1];
- 	document.getElementById("plunge").parentNode.className += " is-focused"
- 	document.getElementById("plunge").value = tp[0];
- 	//	document.getElementById("").innerHTML = Math.round(dir);
- }
+function deviceOrientationHandler(strike, dip, rake) {
 
- //strike dip rake to trend plunge
- function sdrtotp(strike, dip, rake, op_flag) {
- 	/*if ((op_flag === true) && rake != 90) {
- 		strike += 180;
- 		rake = 180 - rake;
- 	}*/
- 	//plunge
- 	var Temp_Pl = Math.asin(Math.sin(torad(dip)) * Math.sin(torad(rake)));
- 	//trend
- 	var Temp_Tr = torad(strike) + Math.atan(Math.cos(torad(dip)) * Math.tan(torad(rake)));
+	document.getElementById("strike").parentNode.classList.add("is-dirty");
+	document.getElementById("strike").value = strike;
+	document.getElementById("dip").parentNode.classList.add("is-dirty");
+	document.getElementById("dip").value = dip;
+	var tp = sdrtotp(strike, dip, rake);
+	document.getElementById("trend").parentNode.classList.add("is-dirty");
+	document.getElementById("trend").value = tp[1];
+	document.getElementById("plunge").parentNode.classList.add("is-dirty");
+	document.getElementById("plunge").value = tp[0];
+	//	document.getElementById("").innerHTML = Math.round(dir);
+}
+
+//strike dip rake to trend plunge
+function sdrtotp(strike, dip, rake, op_flag) {
+	/*if ((op_flag === true) && rake != 90) {
+		strike += 180;
+		rake = 180 - rake;
+	}*/
+	//plunge
+	var Temp_Pl = Math.asin(Math.sin(torad(dip)) * Math.sin(torad(rake)));
+	//trend
+	var Temp_Tr = torad(strike) + Math.atan(Math.cos(torad(dip)) * Math.tan(torad(rake)));
 	getLocation();
- 	return [todeg(Temp_Pl), todeg(Temp_Tr)];
+	return [todeg(Temp_Pl), todeg(Temp_Tr)];
 
- }
+}
 
- function getLocation() {
- 	if (navigator.geolocation) {
- 		navigator.geolocation.watchPosition(showPosition);
- 	} else {
- 		console.log("Geolocation is not supported by this browser.");
- 	}
- }
+function showError(error) {
+	switch (error.code) {
+		case error.PERMISSION_DENIED:
+			document.getElementById("Accpl").innerHTML = "User denied the request for Geolocation.";
+			document.getElementById("Accln").innerHTML = "User denied the request for Geolocation.";
+			break;
+		case error.POSITION_UNAVAILABLE:
+			document.getElementById("Accpl").innerHTML = "Location information is unavailable.";
+			document.getElementById("Accln").innerHTML = "Location information is unavailable.";
+			break;
+		case error.TIMEOUT:
+			document.getElementById("Accpl").innerHTML = "The request to get user location timed out.";
+			document.getElementById("Accln").innerHTML = "The request to get user location timed out.";
+			break;
+		case error.UNKNOWN_ERROR:
+			document.getElementById("Accpl").innerHTML = "An unknown error occurred.";
+			document.getElementById("Accln").innerHTML = "An unknown error occurred.";
+			break;
+	}
+}
 
- function showPosition(position) {
- 	document.getElementById("Planelat").parentNode.className += " is-focused";
- 	document.getElementById("Planelon").parentNode.className += " is-focused";
- 	document.getElementById("Planelat").value = position.coords.latitude;
- 	document.getElementById("Planelon").value = position.coords.longitude;
- 	document.getElementById("Linelat").parentNode.className += " is-focused"
- 	document.getElementById("Linelon").parentNode.className += " is-focused"
- 	document.getElementById("Linelat").value = position.coords.latitude;
- 	document.getElementById("Linelon").value = position.coords.longitude;
- }
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.watchPosition(showPosition)
+	} else {
+		document.getElementById("Accpn").innerHTML = "Geolocation is not supported by this browser.";
+		document.getElementById("Accln").innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+
+function showPosition(position) {
+	document.getElementById("Planelat").parentNode.classList.add("is-dirty");
+	document.getElementById("Planelon").parentNode.classList.add("is-dirty");
+	document.getElementById("Linelat").parentNode.classList.add("is-dirty");
+	document.getElementById("Linelon").parentNode.classList.add("is-dirty");
+
+
+	document.getElementById("Planelat").value = position.coords.latitude;
+	document.getElementById("Planelon").value = position.coords.longitude;
+	document.getElementById("Linelat").value = position.coords.latitude;
+	document.getElementById("Linelon").value = position.coords.longitude;
+
+	document.getElementById("Accpl").innerHTML = position.coords.accuracy;
+	document.getElementById("Accln").innerHTML = position.coords.accuracy;
+}
