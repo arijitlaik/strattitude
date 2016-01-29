cd3db64609d80337e32e2dd17e6f9e5eb1c03977
arijitlaik <arijit.netcom@gmail.com>
2016-01-29 18:50:14 +0530

    new logo

--- js/orient.js
+++ js/orient.js
@@ -7,17 +7,17 @@ function getOrienation() {
 		// Listen for the deviceorientation event and handle the raw data
 		window.addEventListener('deviceorientation', function (eventData) {
 			// gamma is the left-to-right tilt in degrees, where right is positive
-			var rake = eventData.gamma;
+			var plunge = eventData.gamma;
 
 			// beta is the front-to-back tilt in degrees, where front is positive
 			var dip = eventData.beta;
 
 			// alpha is the compass direction the device is facing in degrees
 			var strike = eventData.alpha;
-
+			console.log(eventData.absolute);
 			// call our orientation event handler
 
-			deviceOrientationHandler(strike, dip, rake);
+			deviceOrientationHandler(strike, dip, plunge);
 		}, false);
 	} else {
 		//document.getElementById("doEvent").innerHTML = "Not supported on your device or browser.  Sorry."
@@ -33,32 +33,17 @@ function todeg(radians) {
 	return radians * 180 / Math.PI;
 }
 
-function deviceOrientationHandler(strike, dip, rake) {
+function deviceOrientationHandler(strike, dip, plunge) {
 
 	document.getElementById("strike").parentNode.classList.add("is-dirty");
 	document.getElementById("strike").value = strike;
 	document.getElementById("dip").parentNode.classList.add("is-dirty");
 	document.getElementById("dip").value = dip;
-	var tp = sdrtotp(strike, dip, rake);
-	document.getElementById("trend").parentNode.classList.add("is-dirty");
-	document.getElementById("trend").value = tp[1];
-	document.getElementById("plunge").parentNode.classList.add("is-dirty");
-	document.getElementById("plunge").value = tp[0];
-	//	document.getElementById("").innerHTML = Math.round(dir);
-}
 
-//strike dip rake to trend plunge
-function sdrtotp(strike, dip, rake, op_flag) {
-	/*if ((op_flag === true) && rake != 90) {
-		strike += 180;
-		rake = 180 - rake;
-	}*/
-	//plunge
-	var Temp_Pl = Math.asin(Math.sin(torad(dip)) * Math.sin(torad(rake)));
-	//trend
-	var Temp_Tr = torad(strike) + Math.atan(Math.cos(torad(dip)) * Math.tan(torad(rake)));
-	getLocation();
-	return [todeg(Temp_Pl), todeg(Temp_Tr)];
+	document.getElementById("trend").parentNode.classList.add("is-dirty");
+	document.getElementById("trend").value = strike;
+	document.getElementById("plunge").parentNode.classList.add("is-dirty");
+	document.getElementById("plunge").value = plunge;
 
 }
 
